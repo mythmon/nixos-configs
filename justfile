5b9a7850ec6a75e41f41f3e9254262cf5a7c749d
Michael Cooper <mythmon@gmail.com>
2024-03-03 11:12:02 -0800

    justfile

---
+++ justfile
@@ -0,0 +1,5 @@
+switch:
+  sudo nixos-rebuild switch --flake '.#'
+
+fmt:
+  nix fmt
--- modules/standard/default.nix
+++ modules/standard/default.nix
@@ -1,3 +1,7 @@
-{...}: {
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    just
+  ];
+
   nix.settings.experimental-features = ["nix-command" "flakes"];
 }
