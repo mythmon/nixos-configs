155a60e12418fd33503444fa1a0ce66707149450
Michael Cooper <mythmon@gmail.com>
2024-03-03 20:56:18 -0800

    fonts

--- hosts/fractal/default.nix
+++ hosts/fractal/default.nix
@@ -75,6 +75,19 @@
     xkbVariant = "";
   };
 
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+  ];
+
+
   services.printing.enable = true;
 
   security.rtkit.enable = true;
