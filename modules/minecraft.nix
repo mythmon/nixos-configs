c37fd085711d02ad9b0f830ea68477656ab5e0cc
Michael Cooper <mythmon@gmail.com>
2024-03-16 15:38:47 -0700

    fractal gen 106 - atlauncher overlay at 3.4.35.9

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -4,7 +4,18 @@
   config,
   system,
   ...
-}: {
+}: let
+  overlay-atlauncher = self: super: {
+    atlauncher = super.atlauncher.overrideAttrs (oldAttrs: rec {
+      version = "3.4.35.9";
+      src = super.fetchurl {
+        url = "https://github.com/ATLauncher/ATLauncher/releases/download/v${version}/ATLauncher-${version}.jar";
+        hash = "sha256-Y2MGhzq4IbtjEG+CER+FWU8CY+hn5ehjMOcP02zIsR4=";
+      };
+    });
+  };
+in {
+  nixpkgs.overlays = [overlay-atlauncher];
   home-manager.users.${config.main-user.userName} = {pkgs, ...}: {
     home = {
       packages = with pkgs; [
