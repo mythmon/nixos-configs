b8e6162ef8c8758481079043fb48d98b4a49d52f
Michael Cooper <mythmon@gmail.com>
2024-03-15 23:16:29 -0700

    fractal gen 105 - switching renamed kde variable

--- modules/kde/default.nix
+++ modules/kde/default.nix
@@ -4,16 +4,18 @@
   lib,
   ...
 }: {
-  services.xserver = {
-    enable = true;
-    displayManager = {
-      defaultSession = "plasmax11";
-      sddm = {
-        enable = true;
-        wayland.enable = false;
+  services = {
+    desktopManager.plasma6.enable = true;
+    xserver = {
+      enable = true;
+      displayManager = {
+        defaultSession = "plasmax11";
+        sddm = {
+          enable = true;
+          wayland.enable = false;
+        };
       };
     };
-    desktopManager.plasma6.enable = true;
   };
   programs.dconf.enable = true;
 }
