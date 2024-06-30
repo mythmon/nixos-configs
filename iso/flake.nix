1ee0a0ae186e46ca4c8fdb62f28029218f239ade
Michael Cooper <mythmon@gmail.com>
2024-06-30 10:14:29 -0700

    draft of a custom iso

---
+++ iso/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Minimal NixOS installation media";
+  inputs.nixos.url = "nixpkgs/24.05";
+  outputs = { self, nixos }: {
+    nixosConfigurations = {
+      exampleIso = nixos.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ({ pkgs, ... }: {
+            environment.systemPackages = with pkgs; [ vim git ];
+            isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+          })
+        ];
+      };
+    };
+  };
+}
--- justfile
+++ justfile
@@ -15,3 +15,6 @@ gc:
 
 diff:
   bash -c 'nix run nixpkgs#nix-diff -- $(nix-store -qd $(echo /nix/var/nix/profiles/* | tr " " "\n" | grep link | sort -V | tail -n 2))'
+
+iso:
+  nix build ./iso#nixosConfigurations.exampleIso.config.system.build.isoImage
