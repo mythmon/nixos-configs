f97fbb7ed98971532c63d445f4ae7a5e78328f97
Michael Cooper <mythmon@gmail.com>
2024-09-27 08:55:09 -0700

    fractal gen 23 - updates

--- modules/games/minecraft/default.nix
+++ modules/games/minecraft/default.nix
@@ -5,15 +5,6 @@
   pkgs,
   ...
 }: let
-  overlay-atlauncher-version = self: super: {
-    atlauncher = super.atlauncher.overrideAttrs (oldAttrs: rec {
-      version = "3.4.35.9";
-      src = super.fetchurl {
-        url = "https://github.com/ATLauncher/ATLauncher/releases/download/v${version}/ATLauncher-${version}.jar";
-        hash = "sha256-Y2MGhzq4IbtjEG+CER+FWU8CY+hn5ehjMOcP02zIsR4=";
-      };
-    });
-  };
   overlay-atlauncher-steam-run = self: super: {
     atlauncher = super.atlauncher.overrideAttrs (oldAttrs: rec {
       nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [pkgs.steam-run];
@@ -49,7 +40,6 @@
     };
 in {
   nixpkgs.overlays = [
-    overlay-atlauncher-version
     overlay-atlauncher-steam-run
   ];
   home-manager.users.${config.main-user.userName} = {pkgs, ...}: {
