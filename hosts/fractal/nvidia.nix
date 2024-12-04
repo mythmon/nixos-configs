c405390705751ce663636532ce92fb70d17e03df
Michael Cooper <mythmon@gmail.com>
2024-12-03 19:58:39 -0800

    fractal gen 56 - nvidia modprobe attempt

--- hosts/fractal/nvidia.nix
+++ hosts/fractal/nvidia.nix
@@ -20,5 +20,10 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  # https://forum.garudalinux.org/t/issues-with-nvidia-like-not-waking-up-after-suspending-or-other-issues-with-current-version-of-nvidias-drivers/39572
+  boot.extraModprobeConfig = ''
+    options nvidia NVreg_PreserveVideoMemoryAllocations=1
+  '';
+
   environment.systemPackages = with pkgs; [glxinfo mesa-demos nvtopPackages.full];
 }
