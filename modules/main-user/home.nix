130c2936cd85e8a1edbae1d86862b68473082f02
Michael Cooper <mythmon@gmail.com>
2024-12-25 12:01:23 -0800

    configure zed

--- modules/main-user/home.nix
+++ modules/main-user/home.nix
@@ -83,23 +83,38 @@
 
     zed-editor = {
       enable = true;
+      extensions = ["nix" "toml" "elixir" "make" "html" "dockerfile" "git_firefly"];
       userSettings = {
         autosave = "on_focus_change";
         auto_update = false;
         base_keymap = "VSCode";
-
-        languages.Rust.tab_size = 4;
-
-        vim_mode = true;
-        ui_font_size = 16;
         buffer_font_size = 16;
+        cursor_blink = false;
+        format_on_save = "off";
+        languages.Rust.tab_size = 4;
+        preview_tabs.enabled = false;
+        scrollbar.show = "always";
+        show_inline_completions = true;
         theme = {
-          mode = "system";
+          mode = "dark";
           light = "One Light";
           dark = "One Dark";
         };
+        tab_bar.show_nav_history_buttons = false;
         tab_size = 2;
+        tabs.git_status = true;
+        terminal = {
+          dock = "right";
+          option_as_meta = true;
+          copy_on_select = true;
+          env.EDITOR = "vim";
+        };
+        ui_font_size = 16;
         use_autoclose = false;
+        use_auto_surround = false;
+        vim.use_smartcase_find = true;
+        vim_mode = true;
+        wrap_guides = [80 100 120];
       };
     };
   };
@@ -122,6 +137,7 @@
       imv
       krita
       ncdu
+      nil
       nixd
       parallel
       pavucontrol
