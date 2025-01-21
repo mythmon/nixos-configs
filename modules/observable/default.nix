5040682c9362b532cdb0defc8c037c1a650a3b89
Michael Cooper <mythmon@gmail.com>
2025-01-21 14:19:06 -0800

    graphite gen 50 - observable hosts file

--- modules/observable/default.nix
+++ modules/observable/default.nix
@@ -7,4 +7,25 @@
     rambox
     heroku
   ];
+
+  networking.hosts = {
+    "127.0.0.1" = [
+      "api.observable.test"
+      "avatars.test"
+      "connector.test"
+      "custom.test"
+      "dispatch.observable.test"
+      "events.observable.test"
+      "job-manager.observable.test"
+      "login.usercontent.test"
+      "login.worker.test"
+      "observable.test"
+      "observablehq.observablecloud.test"
+      "projects.test"
+      "prompts.test"
+      "usercontent.test"
+      "worker.test"
+      "ws.observable.test"
+    ];
+  };
 }
