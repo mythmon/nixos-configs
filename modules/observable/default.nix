916711410aee56f5db834861b856eaedfc908bb9
Michael Cooper <mythmon@gmail.com>
2025-02-21 10:55:52 -0800

    graphite gen 70 - replace hosts file with dnsmasq

--- modules/observable/default.nix
+++ modules/observable/default.nix
@@ -9,28 +9,13 @@
     postgresql_17_jit
   ];
 
-  networking.hosts = {
-    "127.0.0.1" = [
-      "api.observable.test"
-      "app.observable.test"
-      "avatars.test"
-      "connector.test"
-      "custom.test"
-      "dispatch.observable.test"
-      "events.observable.test"
-      "job-manager.observable.test"
-      "login.usercontent.test"
-      "login.worker.test"
-      "observable.test"
-      "observablehq.observablecloud.test"
-      "projects.test"
-      "prompts.test"
-      "proxy.observable.test"
-      "tldraw.observable.test"
-      "usercontent.test"
-      "worker.test"
-      "ws.observable.test"
-    ];
+  services.dnsmasq = {
+    enable = true;
+    alwaysKeepRunning = true;
+    resolveLocalQueries = true;
+    settings = {
+      address = ["/test/127.0.0.1"];
+    };
   };
 
   virtualisation.docker.autoPrune.flags = ["--all" "--filter" "label=prune=always"];
