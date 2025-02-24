25057ff7aff8bb138cbce685f256cbf81b920114
Michael Cooper <mythmon@gmail.com>
2025-02-24 15:06:19 -0800

    graphite gen 73 - tweak zed config

--- modules/main-user/zed.nix
+++ modules/main-user/zed.nix
@@ -5,11 +5,20 @@ in {
     enable = true;
     extensions = ["nix" "toml" "elixir" "make" "html" "dockerfile" "git_firefly"];
     userSettings = {
+      assistant = {
+        default_model = {
+          provider = "zed.dev";
+          model = "claude-3-5-sonnet-latest";
+        };
+        version = "2";
+      };
       autosave = "on_focus_change";
       auto_update = false;
       base_keymap = "VSCode";
       buffer_font_size = 16;
       cursor_blink = false;
+      double_click_in_multibuffer = "open";
+      features.edit_prediction_provider = "zed";
       format_on_save = "off";
       languages.Rust.tab_size = 4;
       lsp.nil.autoArchive = true;
